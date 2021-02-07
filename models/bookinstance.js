0b11993955bbe0d0ece7982f1c26bd5d1117789c
PaulKapschitzki <kapschitzki.p@web.de>
2021-02-07 11:12:02 +0100

    Installed luxon to format datetime of due_back date in bookinstance model

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,21 +1,29 @@
 var mongoose = require('mongoose');
+const { DateTime } = require('luxon');
 
 var Schema = mongoose.Schema;
 
 var BookInstanceSchema = new Schema(
   {
-    book: {type: Schema.Types.ObjectId, ref: 'BooK', required: true}, // reference to the associated book
+    book: {type: Schema.Types.ObjectId, ref: 'Book', required: true}, // reference to the associated book
     imprint: {type: String, required: true},
     status: {type: String, required: true, enum: ['Available', 'Maintenance', 'Loaned', 'Reserved'], default: 'Maintenance'},
     due_back: {type: Date, default: Date.now}
   }
 );
 
-// Virual for bookinstance's URL
+// Virtual for bookinstance's URL
 BookInstanceSchema
 .virtual('url')
 .get(function () {
-  return '/catalog/bookinstance' + this._id;
+  return '/catalog/bookinstance/' + this._id;
+});
+
+// Virtual for formatted date
+BookInstanceSchema
+.virtual('due_back_formatted')
+.get(function () {
+  return DateTime.fromJSDate(this.due_back).toFormat('dd-MM-yyyy');
 });
 
 // Export model
